506f82b6781f969cae7c1c43453d150126710636
jisyed14 <jisyed14@gmail.com>
2013-08-24 22:19:38 +0000

    - More Materials - A little level decoration with cubes

---
+++ 10SecIn10Sec/Assets/Scripts/Projectile/DestroyOnWallCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnWallCollision : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.CompareTag("Wall"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
