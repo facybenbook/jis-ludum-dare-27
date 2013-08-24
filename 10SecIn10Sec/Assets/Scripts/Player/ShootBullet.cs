40da6b95416fb460f7e2bbb30079b54750ed9f2e
jisyed14 <jisyed14@gmail.com>
2013-08-24 06:37:30 +0000

    Improved controls on Xbox controller

--- 10SecIn10Sec/Assets/Scripts/Player/ShootBullet.cs
+++ 10SecIn10Sec/Assets/Scripts/Player/ShootBullet.cs
@@ -14,7 +14,7 @@ public class ShootBullet : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton0))
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton5))
 		{
 			Instantiate(bulletPrefab, 
 						transform.position + transform.forward * 1.1f, 
